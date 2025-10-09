cf88af9a7652c54ffd8c8c2579244b21cd187837
mixe007-web <mixe007@gmail.com>
2025-10-09 16:27:26 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -7,9 +7,8 @@ const urlsToCache = [
   './index.html',
   './TripA-B.html',
   './manifest.json',
-  './TripA-B.css',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', event => {
